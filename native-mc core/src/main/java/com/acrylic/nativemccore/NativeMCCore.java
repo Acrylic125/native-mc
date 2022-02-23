167fcdf8be421946848d7cb97237d3920f15e286
Acrylic125 <acrylic125email@gmail.com>
2022-02-23 21:40:27 +0800

    Fixed main plugin name // Added plexus-compiler-eclipse to pom.xml

--- native-mc core/src/main/java/com/acrylic/nativemccore/NativeMCCore.java
+++ native-mc core/src/main/java/com/acrylic/nativemccore/NativeMCCore.java
@@ -1,9 +1,10 @@
 package com.acrylic.nativemccore;
 
 import com.acrylic.nativemc1_18.NativeMCManagerImpl;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class NativeMcCore extends JavaPlugin {
+public final class NativeMCCore extends JavaPlugin {
 
     @Override
     public void onEnable() {
